c1ec8cc044f72531574e32dd415c4bb7f2421c79
jongrover <hello@jonathangrover.com>
2016-02-23 15:45:16 -0500

    updates

---
+++ spec/spec_helper.rb
@@ -0,0 +1,33 @@
+GreenOnion.configure do |c|
+  c.skins_dir = './spec/skins'
+  c.threshold = 20
+  c.skin_name = {
+    :match => /[\/]/, 
+    :replace => "_", 
+    :prefix => nil,
+    :root => "root"
+  }
+  c.dimensions = { :width => 1440, :height => 900 }
+  c.driver = :selenium
+end
+
+def create_screenshots
+  GreenOnion.skin_visual('http://localhost:8000/index.html')
+  GreenOnion.skin_visual('http://localhost:8000/real-estate-listings.html')
+  GreenOnion.skin_visual('http://localhost:8000/market-report.html')
+  GreenOnion.skin_visual('http://localhost:8000/new-properties.html.html')
+end
+
+def start_server_thread
+  @server_thread = Thread.new do 
+    root = Dir.pwd
+    server = WEBrick::HTTPServer.new :Port => 8000, :DocumentRoot => root
+    trap 'INT' do server.shutdown end
+    server.start
+  end
+  sleep 2
+end
+
+def exit_server_thread
+  @server_thread.exit
+end

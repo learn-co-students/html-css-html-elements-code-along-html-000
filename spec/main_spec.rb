4e74c46786c8593f5ea4264a4b8df380379e47da
jongrover <hello@jonathangrover.com>
2016-02-24 16:32:46 -0500

    update spec

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -3,6 +3,7 @@ require 'green_onion'
 require 'capybara/rspec'
 require 'rspec'
 require 'spec_helper'
+require 'pry'
 
 describe 'index.html' do
 
@@ -18,61 +19,41 @@ describe 'index.html' do
 
   before(:each) do
     @comparison = GreenOnion::Compare.new
-    @tolerance = 13.0
+    @tolerance = 0.5
   end
 
   it 'index page should match the solution' do
     solution = './spec/skins/index.html.png'
     student = './spec/skins/index.html_fresh.png'
     @comparison.percentage_diff(solution, student)
-    if (@comparison.percentage_changed > @tolerance)
-      puts "Your page doesn't match our solution."
-      puts "We expected less than #{@tolerance}% difference to the solution and yours was off by #{@comparison.percentage_changed}%."
-    end
-    expect(@comparison.percentage_changed).to be <= @tolerance
+    expect(@comparison.percentage_changed).to be < @tolerance
   end
 
   it 'real estate listings page should match the solution' do
     solution = './spec/skins/real-estate-listings.html.png'
     student = './spec/skins/real-estate-listings.html_fresh.png'
     @comparison.percentage_diff(solution, student)
-   if (@comparison.percentage_changed > @tolerance)
-      puts "Your page doesn't match our solution."
-      puts "We expected less than #{@tolerance}% difference to the solution and yours was off by #{@comparison.percentage_changed}%."
-    end
-    expect(@comparison.percentage_changed).to be <= @tolerance
+    expect(@comparison.percentage_changed).to be < @tolerance
   end
 
   it 'market report page should match the solution' do
     solution = './spec/skins/market-report.html.png'
     student = './spec/skins/market-report.html_fresh.png'
     @comparison.percentage_diff(solution, student)
-    if (@comparison.percentage_changed > @tolerance)
-      puts "Your page doesn't match our solution."
-      puts "We expected less than #{@tolerance}% difference to the solution and yours was off by #{@comparison.percentage_changed}%."
-    end
-    expect(@comparison.percentage_changed).to be <= @tolerance
+    expect(@comparison.percentage_changed).to be < @tolerance
   end
 
   it 'new properties page should match the solution' do
     solution = './spec/skins/new-properties.html.png'
     student = './spec/skins/new-properties.html_fresh.png'
     @comparison.percentage_diff(solution, student)
-    if (@comparison.percentage_changed > @tolerance)
-      puts "Your page doesn't match our solution."
-      puts "We expected less than #{@tolerance}% difference to the solution and yours was off by #{@comparison.percentage_changed}%."
-    end
-    expect(@comparison.percentage_changed).to be <= @tolerance
+    expect(@comparison.percentage_changed).to be < @tolerance
   end
 
   it 'contact page should match the solution' do
     solution = './spec/skins/contact.html.png'
     student = './spec/skins/contact.html_fresh.png'
     @comparison.percentage_diff(solution, student)
-    if (@comparison.percentage_changed > @tolerance)
-      puts "Your page doesn't match our solution."
-      puts "We expected less than #{@tolerance}% difference to the solution and yours was off by #{@comparison.percentage_changed}%."
-    end
-    expect(@comparison.percentage_changed).to be <= @tolerance
+    expect(@comparison.percentage_changed).to be < @tolerance
   end
 end
